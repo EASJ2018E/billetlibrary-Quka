85c879a3db86c054c2fa066bf0d573d879251734
Arlind Ukshini <quka93@gmail.com>
2018-09-12 16:22:25 +0200

    added documentation

--- BilletLibrary/Bil.cs
+++ BilletLibrary/Bil.cs
@@ -4,16 +4,27 @@ using System.Text;
 
 namespace BilletLibrary
 {
+	/// <summary>
+	/// Creates a Bil object with price and vehicle type information
+	/// </summary>
 	public class Bil
 	{
 		public string Nummerplade { get; set; }
 		public DateTime Dato { get; set; }
 
+		/// <summary>
+		/// Get the price of the car
+		/// </summary>
+		/// <returns>Returns 240 as a decimal</returns>
 		public decimal Pris()
 		{
 			return 240;
 		}
 
+		/// <summary>
+		/// Get the type of the vehicle
+		/// </summary>
+		/// <returns>Returns Bil as a string</returns>
 		public string Køretøj()
 		{
 			return "Bil";
