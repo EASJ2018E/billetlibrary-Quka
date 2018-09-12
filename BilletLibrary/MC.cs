8a19778e15d62aacd0400075caf3c2bf2990da63
Arlind Ukshini <quka93@gmail.com>
2018-09-12 16:24:41 +0200

    add MC class with docs

---
+++ BilletLibrary/MC.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BilletLibrary
+{
+	/// <summary>
+	/// Creates an MC object with price and vehicle type information
+	/// </summary>
+	public class MC
+	{
+		public string Nummerplade { get; set; }
+		public DateTime Dato { get; set; }
+
+		/// <summary>
+		/// Get the price of the MC
+		/// </summary>
+		/// <returns>Returns 125 as a decimal</returns>
+		public decimal Pris()
+		{
+			return 125;
+		}
+
+		/// <summary>
+		/// Get the type of the vehicle
+		/// </summary>
+		/// <returns>Returns MC as a string</returns>
+		public string Køretøj()
+		{
+			return "MC";
+		}
+	}
+}
