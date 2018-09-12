396773ac9c84b84b16b6100252982a10ef0838a3
Arlind Ukshini <quka93@gmail.com>
2018-09-12 16:26:23 +0200

    add MC tests

---
+++ BilletLibrary.Tests/TestMC.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BilletLibrary.Tests
+{
+	[TestClass]
+	public class TestMC
+	{
+		[TestMethod]
+		public void TestMCPris()
+		{
+			// Arrange
+			MC mc = new MC();
+
+			// Act
+			decimal pris = mc.Pris();
+
+			// Assert
+			Assert.AreEqual(125, pris);
+		}
+
+		[TestMethod]
+		public void TestMCKøretøj()
+		{
+			// Arrange
+			MC mc = new MC();
+
+			// Act
+			string køretøj = mc.Køretøj();
+
+			// Assert
+			Assert.AreEqual("MC", køretøj);
+		}
+	}
+}
