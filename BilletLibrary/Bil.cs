f1c20075f33e73d03e49b6678df54758acc69860
Arlind Ukshini <quka93@gmail.com>
2018-09-12 16:13:38 +0200

    add project and bil class

---
+++ BilletLibrary/Bil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BilletLibrary
+{
+	class Bil
+	{
+		public string Nummerplade { get; set; }
+		public DateTime Dato { get; set; }
+
+		public decimal Pris()
+		{
+			return 240;
+		}
+
+		public string Køretøj()
+		{
+			return "Bil";
+		}
+	}
+}
